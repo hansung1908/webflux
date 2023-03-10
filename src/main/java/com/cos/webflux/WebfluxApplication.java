2cd93c8e2a0cbbae22517b1a86de34417b198614
hansung1908 <hansung1908@naver.com>
2023-03-10 22:48:31 +0900

    Revert "r2dbc와 h2database를 이용한 webflux 코딩"

--- src/main/java/com/cos/webflux/WebfluxApplication.java
+++ src/main/java/com/cos/webflux/WebfluxApplication.java
@@ -4,8 +4,6 @@ import com.cos.webflux.reactivetest.MyTestPub;
 import com.cos.webflux.reactivetest.MyTestSub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
 // Webflux = 단일 스레드, 비동기 + Stream을 통해 백프레셔가 적용된 데이터만큼 간헐적 응답이 가능하다 + 데이터 소비가 끝나면 응답이 종료
 // SSE 적용 (Servlet, Webflux) = 데이터 소비가 끝나도 Stream 계속 유지
@@ -15,10 +13,11 @@ public class WebfluxApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebfluxApplication.class, args);
 
+		MyTestPub pub = new MyTestPub(); // 신문사 생성
+		MyTestSub sub = new MyTestSub(); // 구독자 생성
 
-//		MyTestPub pub = new MyTestPub(); // 신문사 생성
-//		MyTestSub sub = new MyTestSub(); // 구독자 생성
-//
-//		pub.subscribe(sub);
+		pub.subscribe(sub);
 	}
+
+
 }
